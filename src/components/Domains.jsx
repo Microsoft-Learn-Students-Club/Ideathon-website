c7bf8560e43994d66c4a181d440c072da2871a54
krishachikka <chikkakrisha@gmail.com>
2025-09-01 21:06:18 +0530

    done with about section

--- src/components/Domains.jsx
+++ src/components/Domains.jsx
@@ -74,7 +74,7 @@ const Domains = () => {
 
                         return (
                             <motion.div
-                       /* Then global styles */         key={index}
+                                key={index}
                                 initial={{ opacity: 0, y: 40 }}
                                 whileInView={{ opacity: 1, y: 0 }}
                                 transition={{ duration: 0.6, delay: index * 0.2 }}
