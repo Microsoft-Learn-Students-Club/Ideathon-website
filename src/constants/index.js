87a3ce022bf64b6b6c6cf4d8e4b99a0aa0b33517
VEDANTDHAVAN <vedant.231313108@vcet.edu.in>
2025-09-02 19:50:22 +0530

    updated Navbar routing to general-guidlines.

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,7 +9,7 @@ export const NAV_LINKS = [
     hoverStyles: { bgColor: "#CE2012", textColor: "#ffffff" } },
   { id: "prizes", label: "Prizes", type: "scroll", rotation: 8,
     hoverStyles: { bgColor: "#94D2BD", textColor: "#ffffff" }, },
-  { id: "guidelines", label: "Guidelines", type: "scroll", rotation: -8,
+  { id: "general-guidelines", label: "Guidelines", type: "scroll", rotation: -8,
     hoverStyles: { bgColor: "#0A9396", textColor: "#ffffff" }, },
   { id: "faqs", label: "FAQs", type: "scroll", rotation: 8,
     hoverStyles: { bgColor: "#C49F31", textColor: "#ffffff" }, },
