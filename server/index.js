3887cdcf8390c8677d83874310607556f0b7dea3
Yash Thakur <yashthakur99755@gmail.com>
2025-09-05 12:19:32 +0530

    Register server

---
+++ server/index.js
@@ -0,0 +1,25 @@
+let express=require("express");  //to create server
+let mongoose=require("mongoose"); //to connect to mongodb
+let cors=require("cors"); //to handle cors error
+const Enquiryrouter = require("./App/routes/register.routes");
+let app=express(); //instance of express
+require("dotenv").config(); //to use .env file
+
+app.use(cors())  //to handle cors error
+
+app.use(express.json()) //to parse json data
+
+//routes
+app.use("/register",Enquiryrouter);
+
+app.use("/register",Enquiryrouter);
+
+
+
+//database connection
+mongoose.connect(process.env.DBURL).then(()=>{
+    console.log("Connected to database")
+    app.listen(process.env.PORT);            //to start server
+}).catch((err)=>{
+    console.log("Error connecting to database",err)
+})
